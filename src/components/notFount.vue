92504bd656ceeb9f77e699a74aa3a4000f37acc1
FengJing <448195466@qq.com>
2018-05-19 23:03:17 +0800

    细节优化

---
+++ src/components/notFount.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>页面去旅游了，{{time}}秒后返回主页</h1>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      time: 3
+    }
+  },
+  mounted () {
+    this.time = 3;
+    let timer = setInterval(() => {
+      if(this.time > 0){
+        this.time--
+      }else{
+        this.$router.push({path: '/'})
+        clearInterval(timer)
+        timer = null
+      }
+    },1000)
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    text-align: center;
+    margin-top:100px;
+  }
+</style>
